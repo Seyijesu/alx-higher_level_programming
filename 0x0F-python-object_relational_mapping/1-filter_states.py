c5a26152419a399094596ca5c1ab372076084328
Seyijesu <olalekeoluwaseyi@gmail.com>
2023-06-19 23:05:31 +0100

    sql select statement edited

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,10 @@ if __name__ == '__main__':
                              user=sys.argv[1], passwd=sys.argv[2],
                              db=sys.argv[3])
         cur = db.cursor()
-        cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+        cur.execute("""SELECT * FROM states
+        WHERE name LIKE _utf8mb4 'N%'
+        COLLATE utf8mb4_0900_as_cs
+        ORDER BY id ASC""")
         ct = cur.fetchall()
         for state in ct:
             print(state)
