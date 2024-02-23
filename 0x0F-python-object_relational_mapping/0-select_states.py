2d5289ac55c5d282b909fc2ada6ddf640dcb0b88
Seyijesu <olalekeoluwaseyi@gmail.com>
2024-02-23 23:13:54 +0100

    resolved

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
+''' Lists all states from databases'''
 
 
-import MySQLdb
-from sys import argv
-
-'''
-a script that lists all states
-from the database
-'''
 if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
-    cursor.close()
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cr = db.cursor()
+    cr.execute("SELECT * from states ORDER BY states.id")
+    states = cr.fetchall()
+    for state in states:
+        print(state)
+    cr.close()
     db.close()
-
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
 
-
-import MySQLdb
-from sys import argv
-
-'''
-script that lists all states from the database
-'''
 if __name__ == "__main__":
-    cont = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = cont.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-        (argv[4],)
-        )
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states "
+                "WHERE name = %s "
+                "COLLATE 'latin1_general_cs' "
+                "ORDER BY id ASC", (sys.argv[4],))
+    for r in cur.fetchall():
+        print(r)
+    cur.close()
+    db.close()
